d9882ef641ca3d3def9436cc9d5284b22263de32
meron <meron@example.com>
2022-11-16 09:13:12 -0800

    second try

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,12 +7,13 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+binary_tree_t *newnode;
 newnode = malloc(sizeof(binary_tree_t));
 if (newnode == NULL)
 return (NULL);
-newnode->parent == parent;
+newnode->parent = parent;
 newnode->n = value;
-newnode->left == NULL;
-newnode->right == NULL;
+newnode->left = NULL;
+newnode->right = NULL;
 return (newnode);
 }
