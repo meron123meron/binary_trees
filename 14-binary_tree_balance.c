f1c9f3166aa0211737d862d72f22708cdaf702e9
meron nigussie <merontafer@gmail.com>
2022-11-17 07:04:39 +0300

    modified

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,3 +12,20 @@ return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 return (0);
 }
+/**
+ * binary_tree_height - a function that measures
+ * the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height
+ * Return: height or 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+if (tree)
+{
+size_t l = 0, r = 0;
+l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+return ((l > r) ? l : r);
+}
+return (0);
+}
